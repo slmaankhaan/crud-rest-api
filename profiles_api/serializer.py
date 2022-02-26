28acbdac7845142c4ac479c62a9a7dc8dc8a1996
slmaankhaan <slmaan.khaan@outlook.com>
2022-02-26 12:45:25 -0500

    Read me file added, and app put in structure.

--- profiles_api/serializer.py
+++
@@ -1,5 +0,0 @@
-from rest_framework import serializers
-
-class HelloSerializer(serializers.Serializer):
-    """serializes a name field for testing our APIView"""
-    name = serializers.CharField(max_length=10)
--- profiles_api/urls.py
+++
@@ -1,9 +0,0 @@
-
-from django.urls import path
-
-from profiles_api import views
-
-
-urlpatterns = [
-    path('hello-view/', views.HelloApiView.as_view()),
-]
